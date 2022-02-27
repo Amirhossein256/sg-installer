c09c9e4a820eb81830d27163761515f13c29ffc1
arianomrani <arianomrani24@gmail.com>
2022-02-27 13:31:12 +0330

    update ini file and bug fix

--- sourcegurdian_installer.sh
+++ sourcegurdian_installer.sh
@@ -2,7 +2,6 @@
 
 clear
 echo "###############################################################################"
-echo "start sourcegurdian installation"
 
 # Show an error and exit
 abort() {
@@ -17,24 +16,26 @@ A=(${PHP_VERTION//\./})
 PHP_VERTION_NO_DOT="${A[@]}"
 
 PHP_INI=/usr/local/php$PHP_VERTION_NO_DOT/lib/php.ini
+DIRECTADMIN_INI=/usr/local/php$PHP_VERTION_NO_DOT/lib/php.conf.d/10-directadmin.ini
 
 SOURCE_GUARDIAN_FILE_NAME=SourceGuardian-loaders.linux-x86_64-13.0.zip
 
-#check extension in php.ini file
-if [[ ! "$(grep -P "ixed.\d+\.\d+.lin"  /usr/local/php$PHP_VERTION/lib/php.ini)" ]]; then
-  echo "add extension=ixed.$PHP_VERTION_NO_DOT.lin to php.ini"
-  echo extension=ixed.$PHP_VERTION.lin >> /usr/local/php$PHP_VERTION_NO_DOT/lib/php.ini
-else
-  echo "update sourcegurdian"
+#remove extension from ini files
+sed -i -r '/extension=ixed/d' $PHP_INI 
+sed -i -r '/extension=ixed/d' $DIRECTADMIN_INI
+
+#check extension in ini file
+if [[ ! "$(grep -P "ixed.\d+\.\d+.lin"  $PHP_INI)" ]] || [[ ! "$(grep -P "ixed.\d+\.\d+.lin"  $DIRECTADMIN_INI)" ]]; then
+  #echo "add extension=ixed.$PHP_VERTION_NO_DOT.lin to php.ini"
+  echo extension=ixed.$PHP_VERTION.lin >> $DIRECTADMIN_INI
 fi
 
 cd /usr/local/php$PHP_VERTION_NO_DOT/lib/php/extensions/no-debug-non-zts-*/
 
 #download sourcegurdian file and extraxt it
-echo "download and extraxt sourcegurdian files"
+#echo "download and extraxt sourcegurdian files"
 wget https://bash-files.s3.ir-thr-at1.arvanstorage.com/$SOURCE_GUARDIAN_FILE_NAME >/dev/null 2>&1
 unzip -o $SOURCE_GUARDIAN_FILE_NAME >/dev/null 2>&1
 
 echo " "
-echo "done, for complete installation please"
-echo "restart php handler and webserver"
+echo "done,restart handler and webserver"
